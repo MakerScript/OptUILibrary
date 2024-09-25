38469d52bdd9f520d83ac9a731aaa38c53ba8f3a
MakerScript <104077050+MakerScript@users.noreply.github.com>
2024-09-25 21:50:45 +0700

    Create webhook_discord.lua

---
+++ webhook_discord.lua
@@ -0,0 +1,21 @@
+local HttpService = game:GetService("HttpService")
+local PlayerLocal = game.Players.LocalPlayer
+local OSTime = os.time()
+local Time = os.date('!*t', OSTime)
+
+    request({
+        Url = "",
+        Method = 'POST',
+        Headers = { ['Content-Type'] = 'application/json' },
+        Body = HttpService:JSONEncode({
+            content = "ASDKFASDKF",
+            embeds = {{
+                title = "**Execute Log**",
+                color = 255,
+                fields = {
+                    { name = "Roblox Profile:", value = "[Click here](https://www.roblox.com/users/" .. PlayerLocal.UserId .. "/profile)\n\n".. PlayerLocal.Name .."", inline = true }
+                },
+                timestamp = string.format('%d-%d-%dT%02d:%02d:%02dZ', Time.year, Time.month, Time.day, Time.hour, Time.min, Time.sec)
+            }}
+        })
+    })
