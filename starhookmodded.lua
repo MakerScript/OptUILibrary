cf4dfe369ade6695fdb336f6ac2c88809a6933b0
MakerScript <104077050+MakerScript@users.noreply.github.com>
2024-10-31 04:20:54 +0700

    Update starhookmodded.lua

--- starhookmodded.lua
+++ starhookmodded.lua
@@ -2921,7 +2921,7 @@ local game_support = {
         BulletBeamName = "GunBeam",
         BulletPath = workspace:FindFirstChild("Ignored") or nil
 	},
-	[17787524861] = {
+	[86355831774491] = {
         Number = 16,
         Name = "Hood Z",
         Remote = "MainEvent",
